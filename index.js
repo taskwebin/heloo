5a83f9eeee73410d9cfe90a95b8b5e4eab4621ac
apzap <78036408+apzap@users.noreply.github.com>
2021-01-28 17:56:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
+var global_json_data;
 $(document).ready(function () {
-  $("#basic-form").validate({
-    rules: {
-      department: {
-        required: true,
-      },
-      dates: {
-        required: true,
-      },
-      title: {
-        required: true,
-      },
-      dater: {
-        required: true,
-      },
-      type: {
-        required: true,
-      },
-    },
-    messages: {
-      department: {
-        minlength: "Name should be at least 3 characters",
-      },
-      age: {
-        required: "Please enter your age",
-        number: "Please enter your age as a numerical value",
-        min: "You must be at least 18 years old",
-      },
-    },
+  $("#submit").click(function () {
+    var department = $("#department").val();
+    var title = $("#title").val();
+    var spec = $("#job-spec").val();
+    var reason = $("#reason").val();
+    const brei = $("#breif").val();
+    var approved = $("#approved").val();
+    var request = $("#request").val();
+    var valid = false;
+
+    if (department.length == "") {
+      $("#p").text("Required");
+      $("#department").focus();
+      return false;
+    } else if (title.length == "") {
+      $("#p2").text("Required");
+      $("#title").focus();
+      return false;
+    } else if (spec.length == "") {
+      $("#p3").text("Required");
+      $("#job-spec").focus();
+      return false;
+    } else if (reason.length == "") {
+      $("#p4").text("Required");
+      $("#reason").focus();
+      return false;
+    } else if (request.length == "") {
+      $("#p6").text("Required");
+      $("#request").focus();
+      return false;
+    } else if (approved.length == "") {
+      $("#p7").text("Required");
+      $("#approved").focus();
+      return false;
+    } else if (document.getElementById("#emp-type").cheked) {
+      valid = true;
+    }
   });
 });
